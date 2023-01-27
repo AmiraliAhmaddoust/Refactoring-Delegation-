270144dfbfbd420d32d36b804808739f8e7d6ca1
AmiraliAhmaddoust <115092236+AmiraliAhmaddoust@users.noreply.github.com>
2023-01-27 17:37:31 +0330

    Update structOfInheritance.cs

--- SA/SA/Delegation/structOfInheritance.cs
+++ SA/SA/Delegation/structOfInheritance.cs
@@ -6,7 +6,19 @@ using System.Threading.Tasks;
 
 namespace SA.Delegation
 {
-    class structOfInheritance
+   public class structOfInheritance
     {
+        public string ClassParent { get; set; }
+        public List<string> InterfaceParennts { get; set; }
+        public string Chilld { get; set; }
+        public bool IsParentAbstarct = false;
+        public bool REfactorable = true;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public bool OpenRecurtionExistInSuperClass = false;
+        public bool IsParentThorwable = false;
+        public bool SynchronizingExist = false;
     }
 }
