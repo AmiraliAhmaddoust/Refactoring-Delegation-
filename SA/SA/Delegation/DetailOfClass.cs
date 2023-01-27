053a36f9211d09f6236df36c5069fe87940d6457
AmiraliAhmaddoust <115092236+AmiraliAhmaddoust@users.noreply.github.com>
2023-01-27 17:37:56 +0330

    Update DetailOfClass.cs

--- SA/SA/Delegation/DetailOfClass.cs
+++ SA/SA/Delegation/DetailOfClass.cs
@@ -6,7 +6,14 @@ using System.Threading.Tasks;
 
 namespace SA.Delegation
 {
-    class DetailOfClass
+   public class DetailOfClass
     {
+        public string ClassName { get; set; }
+        public List<DtailsMehods> dtails { get; set; }
+        public List<DtailsMehods> RemovedInConsdtractourbyDelegation { get; set; }
+        public List<DtailsMehods> ids { get; set; }
+        public List<DtailsMehods> beforeClassName { get; set; }
+        public int StartClassToken { get; set; }
+
     }
 }
