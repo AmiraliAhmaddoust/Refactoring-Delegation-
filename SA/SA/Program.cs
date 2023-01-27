42f1d5b14a59f5f1d1cff5ac4aa790321ee699ce
AmiraliAhmaddoust <115092236+AmiraliAhmaddoust@users.noreply.github.com>
2023-01-27 17:02:43 +0330

    test2

--- SA/SA/Program.cs
+++ SA/SA/Program.cs
@@ -7,7 +7,7 @@ namespace SA
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello e!");
         }
     }
 }
