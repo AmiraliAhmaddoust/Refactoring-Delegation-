524b14202ea816c14622fc606c735569323913a9
AmiraliAhmaddoust <115092236+AmiraliAhmaddoust@users.noreply.github.com>
2023-01-28 11:54:58 +0330

    Update Lexer.cs

--- SA/SA/Lexer/Lexer.cs
+++ SA/SA/Lexer/Lexer.cs
@@ -1,10 +1,10 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using static System.Char;
-
 namespace SA.Lexer
 {
      public class Lexer_
