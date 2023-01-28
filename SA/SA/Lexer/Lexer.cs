c40a1567b71faf0830d3807ee5f8ef1fbee81b50
AmiraliAhmaddoust <115092236+AmiraliAhmaddoust@users.noreply.github.com>
2023-01-28 11:39:44 +0330

    Update Lexer.cs

--- SA/SA/Lexer/Lexer.cs
+++ SA/SA/Lexer/Lexer.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace SA.Lexer
 {
-     public class Lexer
+     public class Lexer_
     {
 
         private CsharpDictionary Dictionary = new CsharpDictionary();
